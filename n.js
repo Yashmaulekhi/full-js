5f77c44f2ef18e95910e7e740c77fcbf658d5b15
Yash Maulekhi <yashmaulekhi63@gmail.com>
2025-11-29 19:32:32 +0530

    update number

---
+++ n.js
@@ -0,0 +1,21 @@
+const score =17
+console.log(score);
+const balance =new  Number(17)
+console.log(balance )
+console.log(balance.toFixed(3) )
+const otherNumber = 123.88999
+console.log(otherNumber.toPrecision(3))
+console.log(Number.MAX_VALUE);
+console.log(Number.Min_VALUE);
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.random())//give value between 0 and 1
+console.log((Math.random()*10)+1)//give value between give  value between 1 to 11
+max= 20
+min= 10
+
+console.log(Math.random()*+(max-min+1)+min)//give value between give  value between 10 to 20
+
