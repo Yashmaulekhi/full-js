00d357213686efa365fa16f384a05103a4f33f93
Yash Maulekhi <yashmaulekhi63@gmail.com>
2025-11-29 21:45:33 +0530

    yes

--- array.js
+++ array.js
@@ -13,3 +13,16 @@ console.log(a.includes(7))
 
 
 console.log(a.indexOf(7))
+const d = ["a","b","c","d"]
+const e = ["e","f","g","h"]
+d.push(e)
+console.log(d)
+let f =d.concat(e)
+console.log(f)
+let g =[...d,...e]//same as concat
+console.log(g)
+const r= g.flat(Infinity)//if you have more arraray inside araay many times and want a selement 
+//so by not using a[][]...., you can use flat to make a arrayelement all and infinite means break it infinite timess as five times if array inside array we can put five
+console.log(r)
+console.log(Array.isArray("Yash"))
+console.log(Array.from("Yash"))
