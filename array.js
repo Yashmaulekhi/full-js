ad579a47541bbd7c636760cab69797a50c71b14d
Yash Maulekhi <yashmaulekhi63@gmail.com>
2025-11-29 19:54:12 +0530

    array

---
+++ array.js
@@ -0,0 +1,15 @@
+let a =[5,6,3,9,8]
+console.log(a)
+console.log(typeof(a))
+a.push(2)
+console.log(a)
+a.pop(3)//pop will ony remove last element
+console.log(a)
+a.shift(3)//shift will take 1st element
+console.log(a)
+a.unshift(7)
+console.log(a)
+console.log(a.includes(7))
+
+
+console.log(a.indexOf(7))
