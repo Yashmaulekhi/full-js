c4eac99c3d2207d56be0574dc080c9e507db7fb2
Yash Maulekhi <yashmaulekhi63@gmail.com>
2025-12-01 22:10:10 +0530

    update

--- function.js
+++ function.js
@@ -1,9 +1,9 @@
-function say(){
+function say() {
     console.log("Y")
     console.log("a")
     console.log("s")
     console.log("h")
-    
+
 }
 say()
 
@@ -11,40 +11,78 @@ say()
 /*function addNumbers(number1,number2){
     console.log(number1+number2)
 }*/
-addNumbers(1,3)//you have to give value of number 1 and 2 to get answer otherwise error came if give string concatination{pass null string array check}with one number
-function addNumbers(number1,number2){
-    
-    return number1 +number2
+addNumbers(1, 3)//you have to give value of number 1 and 2 to get answer otherwise error came if give string concatination{pass null string array check}with one number
+function addNumbers(number1, number2) {
+
+    return number1 + number2
 }
-   const result= addNumbers(1,3)
-  
-    console.log("Result:",result)
-    function loginUserNmame(username="sam"){
-        if(!username){
-            console.log("enter a username")
-            return
-        }
-        return `${username} just logged in`
+const result = addNumbers(1, 3)
+
+console.log("Result:", result)
+function loginUserNmame(username = "sam") {
+    if (!username) {
+        console.log("enter a username")
+        return
     }
-    console.log( loginUserNmame("Yash"))
-    function Calculatecarprice(val1,val2,...num1){
-        return num1
-    }
-    console.log( Calculatecarprice(200,400,500,9900))
-    
-    const myNewArray= [200,400,600,300] 
-    function returnSecondValue(getArray){
-    
-        return getArray[1]
-    }
-    console.log(returnSecondValue(myNewArray))
- const user= {
-    username:"Hitesh",
-    price:199
- }
- 
-    
- function handleObject(anyobject){
+    return `${username} just logged in`
+}
+console.log(loginUserNmame("Yash"))
+function Calculatecarprice(val1, val2, ...num1) {// ...=> three dots are rest operator
+    return num1
+}
+console.log(Calculatecarprice(200, 400, 500, 9900))
+
+const myNewArray = [200, 400, 600, 300]
+function returnSecondValue(getArray) {
+
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))
+const user = {
+    username: "Hitesh",
+    price: 199
+}
+
+
+function handleObject(anyobject) {
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
- }
- handleObject(user)
+}
+handleObject(user)
+//-----------Function inside function--------------------------------------------
+function one() {
+    const username = "hitesh"
+    function two() {
+        const website = "youtube.com"
+        console.log(username);
+    }
+    // console.log(website); as website cant define out of scope
+
+    two()
+}
+one()
+if (true) {
+    const username = "Hitesh"
+    if (username === "Hitesh") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website); as outside scope website is not define
+}
+//console.log(username);as outside scope username is notdefine
+//+++++++++++++++++++INTERESTING++++++++++++++++++++++++++++++++++++++++
+/*function addone(num){
+    return num+1
+}
+addone(5)
+const addtwo=function(num){////differntway of writing function
+    return num+1
+}
+addtwo(5)*/
+addone(5)
+function addone(num) {
+    return num + 1
+}
+//addtwo(5)   *******note error came in this syntax when you put function call at start
+const addtwo = function (num){
+    return num + 1
+}
--- objects.js
+++ objects.js
@@ -12,52 +12,55 @@ console.log(Jsuser["email"])//if you want to see it's use make full name as key 
 const mysem = Symbol("key1")
 const Jsuser = {
     name: "yash",
-    mysem:"mykey1",//if you not use it [] in my sem it  
+    mysem: "mykey1",//if you not use it [] in my sem it  
     age: 18,
     location: "xyz",
-    email:"yashmaulekhi63@gmail.com",
+    email: "yashmaulekhi63@gmail.com",
     isLoggedIn: false,
-    lastLoginDays: ["Monday","Saturday"]}
-    console.log(Jsuser.mysem
-        
-    )//use [] in object in mysem AS [mysem]:"mykey1" and mysem in console.log(jsuser[mysem])
-    const obj1 = {
-        1:"a",2:"b"
-    }
-    const obj2 = {
-        3:"a",4:"b"
-    }
-    const obj4 = {
-        5:"a",6:"b"
-    }
-    
-   //const obj3 = Object.assign({},obj1,obj2)//check this is good methiod as botha are same 
-   // const obj3 = Object.assign(obj1,obj2)//check 
+    lastLoginDays: ["Monday", "Saturday"]
+}
+console.log(Jsuser.mysem
 
-   //const obj3={obj1,obj2} //this make object variable as key value pair
-   //const obj3={..obj1,...obj2} 
-  // console.log(obj3)
-   //const ysdh = new Object()//singelton
-   //const ysdh = {}//non singelton result same
-   const tinderuser = {}
-   tinderuser.name="samay"
-   tinderuser.id="123abc"
-   tinderuser.isloggedin=false
-   console.log(tinderuser)
-   console.log(Object.keys(tinderuser))//in array return
-   console.log(Object.values(tinderuser))
-   console.log(Object.entries(tinderuser))//make each key pair value as an element of array
-   console.log(Object.hasOwnProperty('isloggedin'))//make each key pair value as an element of array
-   //JSON------------------------------------------------------------------
-   const course = {
-       coursename:"BCA",
-       courseprice:"123",
-       courseinstructor:"Aditya"}
-       const {courseinstructor} = course
-       console.log(courseinstructor)//also a method to fetch aditya from object used in react see below function
-   
-     const navbar =  ({company}) => {//there it used destructure object
+)//use [] in object in mysem AS [mysem]:"mykey1" and mysem in console.log(jsuser[mysem])
+const obj1 = {
+    1: "a", 2: "b"
+}
+const obj2 = {
+    3: "a", 4: "b"
+}
+const obj4 = {
+    5: "a", 6: "b"
+}
 
-     }
-     navbar(company="hitesh")
-    
+//const obj3 = Object.assign({},obj1,obj2)//check this is good methiod as botha are same 
+// const obj3 = Object.assign(obj1,obj2)//check 
+
+//const obj3={obj1,obj2} //this make object variable as key value pair
+//const obj3={..obj1,...obj2} 
+// console.log(obj3)
+//const ysdh = new Object()//singelton
+//const ysdh = {}//non singelton result same
+const tinderuser = {}
+tinderuser.name = "samay"
+tinderuser.id = "123abc"
+tinderuser.isloggedin = false
+console.log(tinderuser)
+console.log(Object.keys(tinderuser))//in array return
+console.log(Object.values(tinderuser))
+console.log(Object.entries(tinderuser))//make each key pair value as an element of array
+console.log(Object.hasOwnProperty('isloggedin'))//make each key pair value as an element of array
+//JSON------------------------------------------------------------------
+const course = {
+    coursename: "BCA",
+    courseprice: "123",
+    courseinstructor: "Aditya"
+}
+const { courseinstructor } = course
+console.log("Test "+courseinstructor)//also a method to fetch aditya from object used in react see below function
+
+const navbar = ({ company }) => {//there it used destructure object
+
+}
+navbar(company = "hitesh")
+
+c
