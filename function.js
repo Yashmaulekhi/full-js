84f193bc743f2f6a66b494f44f002f87f1c8fff1
Yash Maulekhi <yashmaulekhi63@gmail.com>
2025-12-01 08:45:44 +0530

    update

---
+++ function.js
@@ -0,0 +1,20 @@
+function say(){
+    console.log("Y")
+    console.log("a")
+    console.log("s")
+    console.log("h")
+    
+}
+say()
+
+/*function addNumbers(number1,number2){
+    console.log(number1+number2)
+}*/
+addNumbers(1,3)//you have to give value of number 1 and 2 to get answer otherwise error came if give string concatination{pass null string array check}with one number
+function addNumbers(number1,number2){
+    
+    return number1 +number2
+}
+   const result= addNumbers(1,3)
+  
+    console.log("Result:",result)
