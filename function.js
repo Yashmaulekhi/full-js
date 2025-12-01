4e2b8cf5a235e438b25dbf4568854cec09cf9dee
Yash Maulekhi <yashmaulekhi63@gmail.com>
2025-12-01 19:46:15 +0530

    update

--- function.js
+++ function.js
@@ -7,6 +7,7 @@ function say(){
 }
 say()
 
+
 /*function addNumbers(number1,number2){
     console.log(number1+number2)
 }*/
@@ -18,3 +19,32 @@ function addNumbers(number1,number2){
    const result= addNumbers(1,3)
   
     console.log("Result:",result)
+    function loginUserNmame(username="sam"){
+        if(!username){
+            console.log("enter a username")
+            return
+        }
+        return `${username} just logged in`
+    }
+    console.log( loginUserNmame("Yash"))
+    function Calculatecarprice(val1,val2,...num1){
+        return num1
+    }
+    console.log( Calculatecarprice(200,400,500,9900))
+    
+    const myNewArray= [200,400,600,300] 
+    function returnSecondValue(getArray){
+    
+        return getArray[1]
+    }
+    console.log(returnSecondValue(myNewArray))
+ const user= {
+    username:"Hitesh",
+    price:199
+ }
+ 
+    
+ function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+ }
+ handleObject(user)
